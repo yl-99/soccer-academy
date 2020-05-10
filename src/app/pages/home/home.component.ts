981a40d457575dab4e17af509266f458c3a269ef
ylxrz <ylxrz@mail.umsl.edu>
2020-05-10 02:09:03 -0500

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,13 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  changeText: boolean;
+  
 
-  constructor() { }
+  
+  constructor() {
+    
+   }
 
   public switch = false;
   
 
   ngOnInit(): void {
+    
   }
+
 
 }
